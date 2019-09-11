58af6e9ed378fbd206c135a55de5933827134046
Steven Hunal <stevehunal@gmail.com>
2019-09-11 21:55:40 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,9 +53,9 @@ def self.find_by_name(name)
   DB[:conn].execute(sql, name)
 end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
+def self.find_by_name(att)
+  sql = "SELECT * FROM #{self.table_name} WHERE #{att}"
+  DB[:conn].execute(sql, att)
 end
 
 end
